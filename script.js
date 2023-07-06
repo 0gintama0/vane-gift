3d7733738bbaf0eb2d0bc762526a527b89eaf0c0
gabriele.capobianco <gabriele.capobianco@addvalue.it>
2023-07-06 13:28:47 +0200

    Primo commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const todo = input.value;
+
+    if (todo) {
+        //TODO add todo
+    }
+})
